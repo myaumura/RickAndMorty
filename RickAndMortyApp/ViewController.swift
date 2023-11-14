7440557604d01fb26ef889e6279d5dba8187ae65
Kirill Gusev <k3dosh@gmail.com>
2023-11-14 12:10:31 +0300

    Init commit

---
+++ RickAndMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Kirill Gusev on 14.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
