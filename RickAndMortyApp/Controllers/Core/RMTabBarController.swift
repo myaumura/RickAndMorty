cb689c311657a06a243df44d080da5ab19a9df94
Kirill Gusev <k3dosh@gmail.com>
2023-11-17 15:52:40 +0300

    Created tabBar

--- RickAndMortyApp/Controllers/Core/RMTabBarController.swift
+++ RickAndMortyApp/Controllers/Core/RMTabBarController.swift
@@ -1,5 +1,5 @@
 //
-//  RMTabBarViewController.swift
+//  RMTabBarController.swift
 //  RickAndMortyApp
 //
 //  Created by Kirill Gusev on 14.11.2023.
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class RMTabBarViewController: UITabBarController {
+final class RMTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupTabs()
     }
-
-
+    
+    private func setupTabs() {
+        
+        let charactersVC = RMCharacterController()
+        let locationsVC = RMLocationViewController()
+        let episodesVC = RMEpisodeViewController()
+        let settingsVC = RMSettingsViewController()
+     
+        charactersVC.navigationItem.largeTitleDisplayMode = .always
+        locationsVC.navigationItem.largeTitleDisplayMode = .always
+        episodesVC.navigationItem.largeTitleDisplayMode = .always
+        settingsVC.navigationItem.largeTitleDisplayMode = .always
+        
+        let nav1 = UINavigationController(rootViewController: charactersVC)
+        let nav2 = UINavigationController(rootViewController: locationsVC)
+        let nav3 = UINavigationController(rootViewController: episodesVC)
+        let nav4 = UINavigationController(rootViewController: settingsVC)
+        
+        nav1.tabBarItem = UITabBarItem(title: "Characters", image: UIImage(systemName: "person"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: "Locations", image: UIImage(systemName: "globe"), tag: 2)
+        nav3.tabBarItem = UITabBarItem(title: "Episodes", image: UIImage(systemName: "tv"), tag: 3)
+        nav4.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "gear"), tag: 4)
+        
+        for nav in [nav1, nav2, nav3, nav4] {
+            nav.navigationBar.prefersLargeTitles = true
+        }
+        
+        setViewControllers([nav1, nav2, nav3, nav4], animated: true)
+    }
 }
 
