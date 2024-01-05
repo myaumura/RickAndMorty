786478e4a3cce1c29ef8e88efa7af808b0b04fe0
Kirill Gusev <k3dosh@gmail.com>
2024-01-05 23:13:50 +0300

    SettingView SwiftUI

---
+++ RickAndMortyApp/Views/SwiftUI/RMSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  RMSettingsView.swift
+//  RickAndMortyApp
+//
+//  Created by Kirill Gusev on 05.01.2024.
+//
+
+import SwiftUI
+
+struct RMSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RMSettingsView()
+}
